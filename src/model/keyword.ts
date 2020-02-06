62a667a68bd076121af6d4926590a8f3882dc791
brookmezgu <brookmezgebu64@gmail.com>
2020-02-06 22:53:48 +0300

    fix: ids shouldn't be required on the models, the db should generate them
    * Actually the model should fail if the ids are provided before hand when inserting. (backlog)

--- src/model/keyword.ts
+++ src/model/keyword.ts
@@ -25,7 +25,7 @@ export class Keyword extends Model {
     static get jsonSchema() {
         return {
             type: 'object',
-            required: ['keyword_id' , 'keyword' , 'post_id'],
+            required: ['keyword' , 'post_id'],
             properties: {
                 keyword_id: { type: 'integer'},
                 keyword: { type: 'string'},
