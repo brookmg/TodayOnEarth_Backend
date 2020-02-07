9c443977ccae79ba31fe6f0a2f0f69c092a4d230
brookmezgu <brookmezgebu64@gmail.com>
2020-02-07 12:47:25 +0300

    progress: add arena to the express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 const express =  require('express')
 const json = require('body-parser')
 const cors = require('cors')
+import { Router } from './queue/arena'
 
 const app = express()
 
 app.use(cors())
 app.use(json())
+
+app.use(Router)
 app.get('/' , (req, res) => { res.send({ message: 'Hello' }) })
 
 export function start() {
