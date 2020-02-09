e7c53ea0679e82bdd17c9254a64514254a22a1cd
brookmezgu <brookmezgebu64@gmail.com>
2020-02-09 11:06:51 +0300

    graphql: define all the Metadata types (twitter, fb, insta, telegram)

--- src/graphql/gql.ts
+++ src/graphql/gql.ts
@@ -11,11 +11,116 @@ const typeDef = gql`
         source_link: String, 
         published_on: Int,
         scraped_on: Int,
-        metadata: String
+        metadata: Metadata
     }
 
     type Query {
         getPosts: [Post]
+    }
+
+    type CommunityInteraction {
+        view: Int,
+        likes: Int,
+        replies: Int,
+        retweets: Int,
+        comments: Int,
+        video_views: Int
+    }
+
+    type Owner {
+        owner_id: Int,
+        owner_user_name: String
+    }
+    
+    type AdditionalThumbnails {
+        config_height: Int,
+        config_width: Int,
+        src: String
+    }
+
+    type Dimentions {
+        width: Int,
+        height: Int
+    }
+
+    type Audio {
+        src: String
+    }
+
+    type Image {
+        src: String
+    }
+
+    interface Metadata {
+        community_interaction: CommunityInteraction
+    }
+
+    type FacebookMetadata implements Metadata {
+        community_interaction: CommunityInteraction
+        post: FacebookPost
+    }
+
+    type TwitterMetadata implements Metadata {
+        community_interaction: CommunityInteraction
+        tweet: TwitterPost
+    }
+
+    type TelegramMetadata implements Metadata {
+        community_interaction: CommunityInteraction
+        message: TelegramPost
+    }
+
+    type InstagramMetadata implements Metadata {
+        community_interaction: CommunityInteraction
+        post: InstagramPost
+    }
+
+    interface PostElement {
+        poster_avatar: String,
+        poster_full_name: String,
+        poster_user_name: String,
+    }
+
+    type FacebookPost implements PostElement {
+        poster_avatar: String,
+        poster_media_img: String,
+        poster_media_metadata: String,
+        poster_media_text: String,
+        poster_full_name: String,
+        poster_user_name: String,
+    }
+
+    type TwitterPost implements PostElement {
+        poster_avatar: String,
+        poster_full_name: String,
+        poster_user_name: String,
+    }
+
+    type TelegramPost implements PostElement {
+        poster_avatar: String,
+        poster_full_name: String,
+        poster_user_name: String,
+        owner: Owner,
+        audio: Audio,
+        author_name: String,
+        image: Image,
+    }
+
+    type InstagramPost implements PostElement {
+        poster_avatar: String,
+        poster_media_img: String,
+        poster_media_metadata: String,
+        poster_media_text: String,
+        poster_full_name: String,
+        poster_user_name: String,
+        accessibility_caption: String,
+        owner: Owner,
+        dimensions: Dimentions
+        is_video: Boolean
+        full_size_image: String,
+        additional_thumbnails: [AdditionalThumbnails],
+        post_id: Int,
+        thumbnail_image: String
     }
 
     type Mutation {
@@ -38,6 +143,16 @@ const resolvers = {
         }
     },
 
+    Metadata: {
+        __resolveType(metadata, context, info) {
+            if (metadata.message) return 'TelegramMetadata';
+            else if (metadata.post && metadata.post.additional_thumbnails) return 'InstagramMetadata';
+            else if (metadata.post) return 'FacebookMetadata';
+            else if (metadata.tweet) return 'TwitterMetadata';
+            else return null
+        },
+    },
+
     Post: {
         published_on: (incoming) => {
             return new Date(incoming.published_on).getTime() / 1000
@@ -45,9 +160,9 @@ const resolvers = {
         scraped_on: (incoming) => {
             return new Date(incoming.scraped_on).getTime() / 1000   // sec time instead of micro
         },
-        metadata: (incoming) => {
-            return JSON.stringify(incoming.metadata)
-        }
+        // metadata: (incoming) => {
+        //     return JSON.stringify(incoming.metadata)
+        // }
     },
 
     Mutation: {
