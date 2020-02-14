fbbe8948155232291058ca83dcf4a6c67c578edf
brookmezgu <brookmezgebu64@gmail.com>
2020-02-15 02:06:14 +0300

    progress: add auth router to the express server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-const express =  require('express')
-const json = require('body-parser')
-const cors = require('cors')
+import {authRouter} from "./auth/auth";
+
+const express =  require('express');
+const json = require('body-parser');
+const cors = require('cors');
 import { Router } from './queue/arena'
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(json())
+app.use(cors());
+app.use(json());
 
-app.use(Router)
-app.get('/' , (req, res) => { res.send({ message: 'Hello' }) })
+app.use(Router);
+app.get('/' , (req, res) => { res.send({ message: 'Hello' }) });
+app.use('/auth' , authRouter);
 
 export function start() {
     app.listen(3400, () => {
