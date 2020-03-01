2c958a1d26b4dfc90595c18e0a10956c01c17bc7
brookmezgu <brookmezgebu64@gmail.com>
2020-03-01 21:48:14 +0300

    minor: spell checks

--- src/native/includes/main.h
+++ src/native/includes/main.h
@@ -21,12 +21,12 @@ string toString(json j);
 string getEnvVar( string key ,string defaultValue);
 
 /** TREND.cpp **/
-vector<pair<string, double>> getKeywordFrequency(PyObject* pScoreFunction, json &post,unordered_set<string> &stopWords, bool checkSematicSimilarity);
-void sortByUserInterest(PyObject* pScoreFunction, json &posts,unordered_set<string> &stopWords, vector< pair<std::string,double> > interests, bool checkSemanicSimilarity);
-void sortByTrendingKeyword(PyObject* pScoreFunction,json &posts, unordered_set<string>& stopWords, bool checkSematicSimilarity);
-void tagPostsWithAverageCommunityInteraction(json &posts,unordered_set<string> tagParams);
+vector<pair<string, double>> getKeywordFrequency(PyObject* pScoreFunction, json &post,unordered_set<string> &stopWords, bool checkSemanticSimilarity);
+void sortByUserInterest(PyObject* pScoreFunction, json &posts,unordered_set<string> &stopWords, vector< pair<std::string,double> > interests, bool checkSemanticSimilarity);
+void sortByTrendingKeyword(PyObject* pScoreFunction,json &posts, unordered_set<string>& stopWords, bool checkSemanticSimilarity);
+void tagPostsWithAverageCommunityInteraction(json &posts,unordered_set<string> tagParams);  // not worthy
 void sortByRelativeCommunityInteraction(json &posts,unordered_set<string> scoreParams);
-void tagPostsWithRelativeCommunityInteractionScore(json &posts,unordered_set<string> scoreParams);
+void tagPostsWithRelativeCommunityInteractionScore(json &posts,unordered_set<string> scoreParams); // not worthy
 unordered_map<string, json> getPostCountAndCommunityInteractionByProvider(json &posts);
 void sortByCommunityInteraction(json &posts,unordered_set<string> scoreParams);
 
