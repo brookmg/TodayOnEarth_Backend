9de90d9ef1a92acb0184cc48573d92fd3f90e214
brookmezgu <brookmezgebu64@gmail.com>
2020-03-02 16:07:43 +0300

    minor: change parameter name to reflect the type of input

--- src/native/index.ts
+++ src/native/index.ts
@@ -5,7 +5,7 @@ export class NativeClass {
     public static sortByRelativeCommunityInteraction(posts: string, workingOn: string) { return greet.sortByRelativeCommunityInteraction(posts, workingOn) }
     public static sortByTrendingKeyword(posts: string, workingOn: string, semantics: boolean) { return greet.sortByTrendingKeyword(posts, workingOn, semantics); }
     public static sortByUserInterest(posts: string, userInterests: string, semantics: boolean) { return greet.sortByUserInterest(posts, userInterests, semantics); }
-    public static getKeywordFrequency(posts: string, semantics: boolean) { return greet.getKeywordFrequency(posts, semantics) }
+    public static getKeywordFrequency(post: string, semantics: boolean) { return greet.getKeywordFrequency(post, semantics) }
     public static getPostCountAndCommunityInteractionByProvider(posts: string) {
         return greet.getPostCountAndCommunityInteractionByProvider(posts)
     }
