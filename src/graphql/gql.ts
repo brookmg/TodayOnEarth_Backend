14aae65b30270a6286b6653362cca4bbb4321f31
brookmezgu <brookmezgebu64@gmail.com>
2020-03-06 21:39:06 +0300

    gql: add a query to get today's trending keywords

--- src/graphql/gql.ts
+++ src/graphql/gql.ts
@@ -252,6 +252,7 @@ const typeDef = gql`
         getPostsSortedByCustomKeywords(jsonQuery: [FilterQuery!]!, page: Int, range: Int, orderBy: String, order: String, semantics: Boolean, keywords: [String]): [Post]
         getPostsSortedByTrendingKeyword(jsonQuery: [FilterQuery!]!, page: Int, range: Int, orderBy: String, order: String, semantics: Boolean): [Post]
         getPostTopics(postId: Int, semantics: Boolean) : [Interest]
+        getTodaysTrendingKeywords(semantics: Boolean, page: Int, range: Int) : [Interest]
         
     }
 
@@ -385,6 +386,24 @@ const resolvers = {
             if (!post) throw new Error(`Post doesn't exist`);
             let keywords =  JSON.parse(await NativeClass.getKeywordFrequency(JSON.stringify(post), semantics));
             let returnable = [];
+            keywords.forEach(item => returnable.push({
+                interest: item[0],
+                score: item[1]
+            }));
+            return returnable;
+        },
+        getTodaysTrendingKeywords: async (_ , { semantics, page, range }) => {
+            let posts = await getAllPostsBetweenPublishedDate(
+                (new Date().getTime() - (24 * 60 * 60 * 1000)) / 1000,
+                new Date().getTime() / 1000,
+                page, range
+            );
+
+            if (posts.length == 0) throw new Error('No post were found in the time period');
+            if (posts.length < 2) throw new Error('This method needs at least 2 posts to function');
+
+            let returnable = [];
+            let keywords = JSON.parse(await NativeClass.findKeywordListForPosts(JSON.stringify(posts) , semantics));
             keywords.forEach(item => returnable.push({
                 interest: item[0],
                 score: item[1]
