96f781f1175d2899d5dde8df8db0975f868fb0f4
brookmezgu <brookmezgebu64@gmail.com>
2020-03-07 14:49:12 +0300

    knex: setup knexfile.js to work with knex cli

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : 'toor',
+      database : 'toe'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/knex/staging/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/knex/staging/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : 'toor',
+      database : 'toe'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
