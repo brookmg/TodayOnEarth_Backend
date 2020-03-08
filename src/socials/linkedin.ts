43d1cd2e509f68af14525afb116f57ab3e969cb0
brookmezgu <brookmezgebu64@gmail.com>
2020-03-08 21:23:06 +0300

    socials: add posting feature for linkedin

---
+++ src/socials/linkedin.ts
@@ -0,0 +1,86 @@
+import request from "request-promise-native";
+import {getTokensForUser} from "../db/token_table";
+import {getUser} from "../db/user_table";
+
+async function getLinkedinId (accessToken) {
+    let options = {
+        url: 'https://api.linkedin.com/v2/me',
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'cache-control': 'no-cache',
+            'X-Restli-Protocol-Version': '2.0.0'
+        },
+        strictSSL: true,
+        timeout: 10000
+    };
+
+    try {
+        let response: any = await request(options);
+        console.dir(response);
+        return (JSON.parse(response.body).id)
+    } catch (e) {
+        console.error(e);
+        throw new Error(e)
+    }
+}
+
+async function postShare(accessToken: string, ownerId: string, title: string, text: string, shareUrl: string, shareThumbnailUrl: string) {
+    let body = {
+        "owner": "urn:li:person:" + ownerId,
+        "subject": title,
+        "text": { "text": text }, // max 1300
+        "distribution": {
+            "linkedInDistributionTarget": {}
+        }
+    };
+
+    if (shareUrl) {
+        body["content"] = {
+            "contentEntities": [{ "entityLocation": shareUrl }],
+            "title": title
+        };
+
+        if (shareThumbnailUrl) {
+            body["content"]["contentEntities"] = [{
+                "entityLocation": shareUrl,
+                "thumbnails": [{ "resolvedUrl": shareThumbnailUrl }]
+            }]
+        }
+    }
+
+    let options = {
+        url: 'https://api.linkedin.com/v2/shares',
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'cache-control': 'no-cache',
+            'X-Restli-Protocol-Version': '2.0.0',
+            'Content-Type': 'application/json',
+            'x-li-format': 'json',
+            'Content-Length': Buffer.byteLength(JSON.stringify(body))
+        },
+        body: JSON.stringify(body),
+        strictSSL: true,
+        timeout: 10000
+    };
+
+    try {
+        return await request(options);
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+export async function postOnToLinkedIn(uid: number, title: string, text: string, shareUrl: string = '', shareThumbnailUrl: string = '') {
+    let [accessToken] = (<any>await getTokensForUser(uid))[0].linkedin.split("|||");
+    let linkedInId = (await getUser(uid)).linkedin_id;
+
+    if (!linkedInId) throw new Error('User doesn\'t have a linked LinkedIn account');
+    if (!accessToken) throw new Error('Access Token Invalid');
+
+    postShare(accessToken, linkedInId, title, text, shareUrl, shareThumbnailUrl).then(r => {
+        console.log(r); // status 201 signal successful posting
+    }).catch(e => console.log(e));
+
+}
