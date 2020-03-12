cb37f4c3cb86b53ddab89a4d0ffd38b7a28b8e64
brookmezgu <brookmezgebu64@gmail.com>
2020-03-12 17:03:41 +0300

    socials: add ability to send tweets with images

--- src/socials/twitter.ts
+++ src/socials/twitter.ts
@@ -1,10 +1,16 @@
 import {getTokensForUser} from "../db/token_table";
+
 const Twitter = require('twitter');
+const fs = require('fs');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
-export async function postTweet(tweet: object, uid: number) {
+async function pushMedia(t: any, media: Buffer) : Promise<string> {
+    return (await t.post('media/upload', {media: media})).media_id_string;
+}
+
+export async function postTweet(tweet: any, medias: string[], uid: number) {
     let [accessToken, accessTokenSecret] = (<any>await getTokensForUser(uid))[0].twitter.split("|||");
 
     if (!accessToken || !accessTokenSecret) throw new Error(`User doesn't have active linked twitter account`);
@@ -14,6 +20,16 @@ export async function postTweet(tweet: object, uid: number) {
         access_token_key: accessToken,
         access_token_secret: accessTokenSecret
     });
+
+    if (medias.length > 0) {
+        let mediaBuffers = medias.map(media => fs.readFileSync(media));
+        tweet.media_ids = (await Promise.all(mediaBuffers.map(
+            mediaBuffer => {
+                return pushMedia(T, mediaBuffer);
+            }
+        ))).reduce((i , j) => `${i},${j}`);
+        console.dir(tweet, { depth: null })
+    }
 
     return T.post('statuses/update', tweet);
 }
