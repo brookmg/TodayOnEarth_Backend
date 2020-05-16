be02007b15b7d3b5673deaeddedafed57388405b
Brook Mezgebu <brook.mezgebu.dev@gmail.com>
2020-05-16 12:16:14 +0300

    fix: undo code change

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,8 +4,7 @@ const state = process.env.NODE_ENV || 'development';
 
 // These values might change with some env values
 // This is the instance in which all models get db access from
-//export const KnexI = Knex(require('../../knexfile')[state]);
-export const KnexI = Knex(process.env.DATABASE);
+export const KnexI = Knex(require('../../knexfile')[state]);
 
 
 
