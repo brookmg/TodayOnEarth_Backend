8375ac328dad270a8443dfb571ce60c743bf3748
Brook Mezgebu <brook.mezgebu.dev@gmail.com>
2020-05-16 12:04:32 +0300

    minor: fix db password change from heroku

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,7 +4,8 @@ const state = process.env.NODE_ENV || 'development';
 
 // These values might change with some env values
 // This is the instance in which all models get db access from
-export const KnexI = Knex(require('../../knexfile')[state]);
+//export const KnexI = Knex(require('../../knexfile')[state]);
+export const KnexI = Knex(process.env.DATABASE);
 
 
 
